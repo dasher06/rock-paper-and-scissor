d91067b11e3fbe1b06429f14bc9f528c2e7f3292
dasher06 <121686865+dasher06@users.noreply.github.com>
2022-12-31 11:43:50 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,67 +1,49 @@
 import random
 from getpass import getpass
-a=input("Enter your name ")
-print("Bot: Hi",a,"!")
+
+p1_name=input("Enter your name ")
+print("Bot: Hi",p1_name,"!")
 print("Bot: Ready to play rock ,paper and scissors !")
 print("Bot: There are two modes")
 print("Bot: Bot vs Player(1st) and Player1 vs Player2(2nd)")
 print("Bot: To play 1st mode enter (c)and to play second mode enter any string(letters)")
-b=input(a+": ")
-if b=="C" or b=="c":
-    l=["rock","paper","scissor","Rock","Paper","Scissor"]
+mode=input(p1_name+": ")
+mode=mode.lower()
+p2=""
+p2_name=""
+winner_name=""
+if mode=="c":
+    l=["rock","paper","scissor"]
+    p2_name="Bot"
     print("Bot: I have entered ,it's your turn")
-    p1=random.choice(l)
-    p2=input(a+": ")
-    p3=p2.lower()
-    if (p1==p3):
-        print("Game tied")
-    else:
-        if p1=="rock":
-            if p3=="paper":
-                print(a,"is the winner!!")
-            if p3=="scissor":
-                print("Bot is the winner!1")
-        else:
-            if p1=="paper":
-                if p3=="rock":
-                    print("Bot is the winner!!")
-                if p3=="scissor":
-                    print(a,"is the winner!!")
-            else:
-                if p1=="scissor":
-                    if p3=="paper":
-                        print("Bot is the winner!!")
-                    if p3=="rock":
-                        print(a,"is the winner")
-                else:
-                    print("Wrong input")
+    p2=random.choice(l)
 else:
-    c=input("Bot: Enter second player name ")
-    print("Bot: Now the game is between",a,"and",c,"!!")
-    p3=getpass(a+": ")
-    p5=p3.lower()
-    p4=input(c+": ")
-    p6=p4.lower()
-    if (p5==p6):
-        print("Game tied")
+    p2_name=input("Bot: Enter second player name ")
+    p2=input(p2_name+": ")
+    p2=p2.lower()
+p1=input(p1_name+": ")
+p1=p1.lower() 
+if p1!="paper" and p1!="rock" and p1!="scissor" or p2!="paper" and p2!="rock" and p2!="scissor":
+    print("wrong input")
+else:
+    if(p1==p2):
+        print("Game tied!!")
     else:
-        if p5=="rock":
-            if p6=="paper":
-                print(c,"is the winner!!")
-            if p6=="scissor":
-                print(a,"is the winner!!")
+        if p2=="rock":
+            if p1=="paper":
+                winner_name=p1_name
+            if p1=="scissor":
+                winner_name=p2_name
         else:
-            if p5=="paper":
-                if p6=="rock":
-                    print(a,"is the winner!!")
-                if p6=="scissor":
-                    print(c,"is the winner!!")
+            if p2=="paper":
+                if p1=="rock":
+                    winner_name=p2_name
+                if p1=="scissor":
+                    winner_name=p1_name
             else:
-                if p5=="scissor":
-                    if p6=="paper":
-                        print(a,"is the winner!!")
-                    if p6=="rock":
-                        print(c,"is the winner!!")
-                else:
-                    print("Wrong input") 
-
+                if p2=="scissor":
+                    if p1=="rock":
+                        winner_name=p1_name
+                    if p1=="paper":
+                        winner_name=p2_name
+print(winner_name,"is the winner!!")
