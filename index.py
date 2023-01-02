b7c8f67435d949b145a2218020dfe3c02dceec38
dasher06 <121686865+dasher06@users.noreply.github.com>
2023-01-02 17:45:19 +0530

    Update index.py

--- index.py
+++ index.py
@@ -30,31 +30,49 @@ def rps(p1,p2):
 p1_name=input("Enter your name ")
 print("Bot: Hi",p1_name,"!")
 print("Bot: Ready to play rock ,paper and scissors !")
-print("Bot: There are two modes")
-print("Bot: Bot vs Player(1st) and Player1 vs Player2(2nd)")
-print("Bot: To play 1st mode enter (c)and to play second mode enter any string(letters)")
-mode=input(p1_name+": ")
-mode=mode.lower()
-p2=""
-p2_name=""
-winner_name=""
-if mode=="c":
-    l=["rock","paper","scissor"]
-    p2_name="Bot"
-    print("Bot: I have entered ,it's your turn")
-    p2=random.choice(l)
+true=input(p1_name+": ")
+true=true.lower()
+if true=="yes" or true=="y":
+    while true:
+        print("Bot: There are two modes")
+        print("Bot: Bot vs Player(1st) and Player1 vs Player2(2nd)")
+        print("Bot: To play 1st mode enter (c)and to play second mode enter any string(letters)")
+        mode=input(p1_name+": ")
+        mode=mode.lower()
+        p2=""
+        p2_name=""
+        winner_name=""
+        if mode=="c":
+            l=["rock","paper","scissor"]
+            p2_name="Bot"
+            print("Bot: I have entered ,it's your turn")
+            p2=random.choice(l)
+        else:
+            p2_name=input("Bot: Enter second player name ")
+            p2=getpass(p2_name+": ")
+            p2=p2.lower()
+        p1=input(p1_name+": ")
+        p1=p1.lower()
+        result=rps(p1,p2)
+        if result==1:
+            print("Wrong input")
+        elif result==2:
+            print("Game tied")
+        elif result==3:
+            print(p1_name,"is the winner!!")
+        else:
+            print(p2_name,"is the winner!!")
+        print("Bot: Do you want to play again",p1_name)
+        print("Bot: for yes(y) and for no(n)")
+        ag=input(p1_name+": ")
+        ag=ag.lower()
+        if ag=="yes" or ag=="y":
+            print("Bot: Ok")
+        else:
+            print("Bot: Ok, we'll play next time if you want")
+            break
+elif true=="no" or true=="n":
+    print("Bot: Ok, we'll play next time if you want")
 else:
-    p2_name=input("Bot: Enter second player name ")
-    p2=getpass(p2_name+": ")
-    p2=p2.lower()
-p1=input(p1_name+": ")
-p1=p1.lower()
-result=rps(p1,p2)
-if result==1:
-    print("Wrong input")
-elif result==2:
-    print("Game tied")
-elif result==3:
-    print(p1_name," is the winner!!")
-else:
-    print(p2_name," is the winner!!")
+    print("invalid string",p1_name)
+           
