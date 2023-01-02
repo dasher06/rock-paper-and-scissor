bedbc4a00e8d8246f8f63d514148a9319d77b796
dasher06 <121686865+dasher06@users.noreply.github.com>
2023-01-02 17:10:30 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,32 @@
 import random
 from getpass import getpass
 
+def rps(p1,p2):
+    if p1!="paper" and p1!="rock" and p1!="scissor" or p2!="paper" and p2!="rock" and p2!="scissor":
+        return 1
+    else:
+        if(p1==p2):
+            return 2
+        else:
+            if p2=="rock":
+                if p1=="paper":
+                    return 3
+                if p1=="scissor":
+                    return 4        
+            else:
+                if p2=="paper":
+                    if p1=="rock":
+                        return 4  
+                    if p1=="scissor":
+                        return 3
+                else:
+                    if p2=="scissor":
+                        if p1=="rock":
+                            return 3 
+                        if p1=="paper":
+                            return 4
+                            
+                            
 p1_name=input("Enter your name ")
 print("Bot: Hi",p1_name,"!")
 print("Bot: Ready to play rock ,paper and scissors !")
@@ -19,31 +45,16 @@ if mode=="c":
     p2=random.choice(l)
 else:
     p2_name=input("Bot: Enter second player name ")
-    p2=input(p2_name+": ")
+    p2=getpass(p2_name+": ")
     p2=p2.lower()
 p1=input(p1_name+": ")
-p1=p1.lower() 
-if p1!="paper" and p1!="rock" and p1!="scissor" or p2!="paper" and p2!="rock" and p2!="scissor":
-    print("wrong input")
+p1=p1.lower()
+result=rps(p1,p2)
+if result==1:
+    print("Wrong input")
+elif result==2:
+    print("Game tied")
+elif result==3:
+    print(p1_name," is the winner!!")
 else:
-    if(p1==p2):
-        print("Game tied!!")
-    else:
-        if p2=="rock":
-            if p1=="paper":
-                winner_name=p1_name
-            if p1=="scissor":
-                winner_name=p2_name
-        else:
-            if p2=="paper":
-                if p1=="rock":
-                    winner_name=p2_name
-                if p1=="scissor":
-                    winner_name=p1_name
-            else:
-                if p2=="scissor":
-                    if p1=="rock":
-                        winner_name=p1_name
-                    if p1=="paper":
-                        winner_name=p2_name
-print(winner_name,"is the winner!!")
+    print(p2_name," is the winner!!")
